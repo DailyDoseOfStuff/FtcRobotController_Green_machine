762b9aa7bdacdd9cc820ebed101a78a5d0e37c90
Allen Liu <allenliu@outlook.com>
2024-10-09 21:16:06 -0500

    added field centric and test layout file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test1.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "tutorial", group = "Opmode")
+public class test1 extends OpMode {
+
+    DcMotor motor1, motor2, motor3, motor4;
+    Servo servo1;
+    double pos = 0;
+    double ticks;
+    @Override
+    public void init(){
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+
+}
