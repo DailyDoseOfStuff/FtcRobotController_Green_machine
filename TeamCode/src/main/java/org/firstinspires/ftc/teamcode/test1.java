576a9c25a0a73d911365aae22771fd614708b364
Allen <allenliu@outlook.com>
2024-12-04 23:42:27 -0600

    made it a test for the arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test1.java
@@ -1,20 +1,33 @@
 package org.firstinspires.ftc.teamcode;
 
+import java.util.*;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "test1", group = "LinearOpMode")
 public class test1 extends LinearOpMode {
 
 
-    DcMotor armRotate;
-    Servo servo1;
-    double pos = 0;
-    double ticks = 537.7;
-    DcMotor armExtend;
-    
+    private DcMotor armRotate;
+    private CRServo servo1;
+    private double pos = 0;
+    private static final double ticks = 537.7;
+    private DcMotor armExtend;
+    @Override
+    public void runOpMode(){
+        armRotate = hardwareMap.get(DcMotor.class, "armRotate");
+        servo1 = hardwareMap.get(CRServo.class, "servo1");
+        armExtend = hardwareMap.get(DcMotor.class, "armExtend");
+        if(gamepad1.a){
+            encoder(2);
+        }
+        if(gamepad1.b){
+            armExtend.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        }
+   }
 
     //turn the motor in a circular way make sure its the right way, if not make it negative
     public void encoder(int turnage){
@@ -24,18 +37,4 @@ public class test1 extends LinearOpMode {
         armRotate.setPower(0.5);
         armRotate.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
-
-
-    @Override
-    public void runOpmode(){
-        armRotate = hardwareMap.get(DcMotor.class, "armRotate");
-        servo1 = hardwareMap.get(CRservo.class, "servo1");
-        armExtend = hardwareMap.get(DcMotor.class, "armExtend");
-        if(gamepad1.a){
-            encoder(2);
-        }
-        if(gamepad1.b){
-            armExtend.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        }
-   }
 }
